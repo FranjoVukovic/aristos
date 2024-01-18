bb90c53e51914fc5fcd5426b288c5cbed88d7d39
FranjoVukovic <148588245+FranjoVukovic@users.noreply.github.com>
2024-01-18 18:09:20 +0100

    Update UserController.java

--- IzvorniKod/backend/src/main/java/com/example/backend/korisnik/UserController.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/korisnik/UserController.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@CrossOrigin(origins = "https://tryout-dg6o.onrender.com")
 @RestController
 @RequestMapping
 public class UserController {
